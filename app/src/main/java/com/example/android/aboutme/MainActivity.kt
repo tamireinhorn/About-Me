a284322bc970542ea1232f43b9c0b73747715a05
tamireinhorn <tamtamireinhorn@gmail.com>
2022-01-28 20:31:46 +0200

    Adding on click listener to the button to make it display nickname.

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.android.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener{
+            addNickname(it)
+        }
+    }
+    private fun addNickname(button: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text // Sets the nickname as the text in the edit pane.
+        editText.visibility = View.GONE
+        button.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE //Hides the edit text and the button but now shows the nickname
+
     }
 }
+
+
+
