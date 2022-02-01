d2bfa72ee7e960d0f6745f0dfaaddfada9ca3851
tamireinhorn <tamtamireinhorn@gmail.com>
2022-02-01 17:03:57 +0200

    Added data binding instead of view binding.

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -8,24 +8,26 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener{
-            addNickname(it)
-        }
+        binding.doneButton.setOnClickListener { addNickname(it) }
     }
     private fun addNickname(button: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text // Sets the nickname as the text in the edit pane.
-        editText.visibility = View.GONE
-        button.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE //Hides the edit text and the button but now shows the nickname
+        binding.nicknameText.text =  binding.nicknameEdit.text // Sets the nickname as the text in the edit pane.
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE //Hides the edit text and the button but now shows the nickname
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(button.windowToken, 0)
