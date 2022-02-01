f5d0d0013a8829bda09d83ea189b5335c767ce72
tamireinhorn <tamtamireinhorn@gmail.com>
2022-02-01 17:17:35 +0200

    Added apply to simplify code, refactored a typo in padding name, also centered the nickname view.

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.android.aboutme.databinding.ActivityMainBinding
 
@@ -23,14 +20,17 @@ class MainActivity : AppCompatActivity() {
         binding.doneButton.setOnClickListener { addNickname(it) }
     }
     private fun addNickname(button: View){
+        binding.apply { //Saves us from having to constantly reference the binding.
+            nicknameText.text =  nicknameEdit.text // Sets the nickname as the text in the edit pane.
+            invalidateAll() // Refreshes the bindings.
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE //Hides the edit text and the button but now shows the nickname }
 
-        binding.nicknameText.text =  binding.nicknameEdit.text // Sets the nickname as the text in the edit pane.
-        binding.nicknameEdit.visibility = View.GONE
-        binding.doneButton.visibility = View.GONE
-        binding.nicknameText.visibility = View.VISIBLE //Hides the edit text and the button but now shows the nickname
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(button.windowToken, 0)
+            // Hide the keyboard.
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(button.windowToken, 0)
+        }
     }
 }
 
